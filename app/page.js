90751dda2f6a2c8a60726647a0714d248d149136
fdolyamd-ux <fdoly.amd@gmail.com>
2026-02-21 09:31:10 +0700

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,40 @@
+export default function Home() {
+  return (
+    <main style={{fontFamily:"Arial", margin:0, padding:0}}>
+      
+      <section style={{
+        background:"#0f172a",
+        color:"white",
+        padding:"60px 20px",
+        textAlign:"center"
+      }}>
+        <h1 style={{fontSize:"32px", marginBottom:"10px"}}>
+          SDN KARANGPARI 01
+        </h1>
+        <p>
+          Dukuh Parasi RT 007 RW 001 Desa Karangpari  
+          Kecamatan Bantarkawung Kabupaten Brebes
+        </p>
+        <p>NPSN: 20327208</p>
+      </section>
+
+      <section style={{padding:"40px 20px", textAlign:"center"}}>
+        <h2>Selamat Datang</h2>
+        <p>
+          Website resmi SD Negeri Karangpari 01.
+          Informasi sekolah, pengumuman, kegiatan dan prestasi
+          dapat dilihat di sini.
+        </p>
+      </section>
+
+      <section style={{
+        background:"#e2e8f0",
+        padding:"30px",
+        textAlign:"center"
+      }}>
+        <p>© 2026 SDN Karangpari 01</p>
+      </section>
+
+    </main>
+  )
+}
